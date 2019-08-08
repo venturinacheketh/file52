1d4f7117e819f79f65647f226303f4c57eaf39e2
venturinacheketh <53895193+venturinacheketh@users.noreply.github.com>
2019-08-08 13:46:41 +0530

    swish.c

---
+++ swish.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+      printf("a=list(map(int,input().split(' ')))
+b=a[0]
+c=a[1]
+d=0
+while(b!=0):
+  r=b%c
+  d+=r
+  b=(b/r)
+print(d+b)\n");
+      return 0;
+}
